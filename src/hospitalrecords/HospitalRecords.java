da76c7543fca03686e15f1cc1f676f15601b9bb0
PeterStanger92020362 <psta191@mywhitecliffe.com>
2020-06-15 18:47:38 +1200

    Completed Question 1

--- src/hospitalrecords/HospitalRecords.java
+++ src/hospitalrecords/HospitalRecords.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  *
@@ -26,52 +27,76 @@ public class HospitalRecords {
     
     public static void main(String[] args) {
         
-        //ArrayList<Object> hospitalRecords = new ArrayList<>();
-        
         obtainRecords();
         
-        System.out.println("**Patient records have been recorded successfully**\n");
-        
+        Scanner sc = new Scanner(System.in);
+        String input = "";
 
-        
-        
-        for (Object each: hospitalRecords){
-            System.out.println(each + "\n");
-            each.getName();
+        OUTER:
+        while (true) {
+            System.out.println("\nPress S for Search, Press E for exit");
+            input = sc.nextLine();
+
+            switch (input.toLowerCase()) {
+                case "s":
+                    System.out.println("Enter Patient ID to search\n");
+                    input = sc.nextLine();
+                    System.out.println(searchRecords(input));
+                    break;
+                case "e":
+                    break OUTER;
+                default:
+                    break;
+            }
         }
         
-
-
-
+        System.out.println("Bye...");
         
-        Patient bob = new Patient("b","B","B","B");
-        System.out.println(bob.getPatientID());
-        
+                 
     }
     
 
     
-/*  
-    public String searchRecords(ArrayList recordsArray, String patID) {
+  
+    public static String searchRecords(String patID) {
         
         String foundRecord = "";
         
-        Iterator iterator = recordsArray.iterator();
-        
-        while (iterator.hasNext()) {
-            Object recordPatID;
-            recordPatID = iterator.next();
-            recordPatID.getPatientID();
-            if (each.patientID.equals(patID)){
-                
-            };
-        }
-        
-        
+        for (int i = 0; i < hospitalRecords.size(); i++) {
+                      
+            if (hospitalRecords.get(i).getPatientID().toLowerCase().equals(patID.toLowerCase()) ){
+                System.out.println("\n" + hospitalRecords.get(i).toString() +"\n");
+                int index = i;
+                foundRecord = removeRecord(index);
+                break;
+            } else {
+                foundRecord = "The search found no matching patient.";
+            }
+        }    
+
         return foundRecord;
     }
- */   
     
+    public static String removeRecord(int index){
+        
+        System.out.println("Would you like to remove the patient's record(s)?");
+        System.out.println("Y or N?");
+              
+        String result;
+        
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+              
+        if (input.toLowerCase().equals("y")){
+            hospitalRecords.remove(index);
+            result = "Record(s) removed successfully";
+        } else {
+            result = "No record(s) removed.";
+        }
+        return result;
+    }
+  
+
     
     public static void obtainRecords() {
         
@@ -84,25 +109,24 @@ public class HospitalRecords {
         try {
             //using try-catch for exception handling to catch
             //possible errors with i/o operations
-            FileReader inputStream = new FileReader(path);
-            //FileReader reads the file’s contents
+            FileReader inputStream = new FileReader(path);              //FileReader reads the file’s contents
             lr = new LineNumberReader(inputStream);
             String str;
-            while ((str = lr.readLine()) != null) {
-                //while the next line exists
-                oneRecord = str.split(",");
-                //dividing one file line by commas and assigning to array
+            while ((str = lr.readLine()) != null) {                 //while the next line exists
+                oneRecord = str.split(",");                 //dividing one file line by commas and assigning to array
+                
                 pat = new Patient(oneRecord[0], oneRecord[1],
                         oneRecord[2], oneRecord[3]);
                 
                 hospitalRecords.add(pat);
-
                    
             }
+            System.out.println("**Patient records have been recorded successfully**");
         }
         catch (IOException ioe) {
             System.out.println("IOExcception occured");
         }
+        
         
         
     }    
